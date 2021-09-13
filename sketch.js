aebc888afc5cb684b1cd1a95c72fb86e4a32c798
Rbanik-coder <85229500+Rbanik-coder@users.noreply.github.com>
2021-09-13 21:38:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,93 @@
+var balloon,balloonImage1,balloonImage2;
+var database;
+var height;
+
+function preload(){
+   bg =loadImage("Images/space.jpg");
+   balloonImage1=loadAnimation("Images/balloon.png");
+   balloonImage2=loadAnimation("Images/balloon.png");
+  }
+
+//Function to set initial environment
+function setup() {
+
+   database=firebase.database();
+
+  createCanvas(1520,730);
+
+  balloon=createSprite(250,650,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  var balloonHeight=database.ref('balloon/height');
+  balloonHeight.on("value",readHeight, showError);
+
+
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    updateHeight(-10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    updateHeight(0,+10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale+0.005;
+  }
+
+  drawSprites();
+  fill("darkblue");
+  stroke("cyan");
+  strokeWeight(5)
+  textSize(30);
+  text("Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+ function updateHeight(x,y){
+   database.ref('balloon/height').set({
+     'x': height.x + x ,
+     'y': height.y + y
+   })
+ }
+
+
+//CHOOSE THE CORRECT READHEIGHT FUNCTION
+// function readHeight(data){
+//   balloon.x = height.x;
+//   balloon.y = height.y;
+// }
+
+function readHeight(data){
+ height = data.val();
+ balloon.x = height.x;
+ balloon.y = height.y;
+}
+
+// function readHeight(data){
+//   height = data.val();
+// }
+
+// function readHeight(){
+//   height = val();
+//   balloon.x = height.x;
+//   balloon.y = height.y;
+// }
+
+function showError(){
+  console.log("Error in writing to the database");
+}
